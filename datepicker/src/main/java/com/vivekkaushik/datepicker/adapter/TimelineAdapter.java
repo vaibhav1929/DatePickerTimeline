0e940df8b7a76c5ae1c4399cb6da141ea2ba196e
vaibhav patel <30389552+vaibhav1929@users.noreply.github.com>
2021-11-06 17:31:49 +0530

    Update TimelineAdapter.java

--- datepicker/src/main/java/com/vivekkaushik/datepicker/adapter/TimelineAdapter.java
+++ datepicker/src/main/java/com/vivekkaushik/datepicker/adapter/TimelineAdapter.java
@@ -95,7 +95,7 @@ public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.ViewHo
 
     @Override
     public int getItemCount() {
-        return futureDatesCount;
+        return (futureDatesCount < 0)? Integer.MAX_VALUE:futureDatesCount;
     }
 
     public void disableDates(Date[] dates) {
